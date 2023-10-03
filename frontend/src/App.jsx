2d36b42fbb76548f72da336f24cf8df7c324f700
z5311248 <melanie.wu@student.unsw.edu.au>
2023-10-03 21:26:48 +1100

    F13ATFT-30: Create React App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from './components/Register'
+import CssBaseLine from '@mui/material/CssBaseline'
+
+function App() {
+  return (
+    <BrowserRouter >
+      <CssBaseLine />
+      <Routes>
+        <Route exact path="/" element={<Register />} /> {/* change to login */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
