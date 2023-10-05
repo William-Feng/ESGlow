06677b68e1f24522b181cdd536ed0737f065024b
seanwibi1 <seanwibi.work@gmail.com>
2023-10-06 00:03:56 +1100

    F13AFT-29: Switch to flask-restplus and setup registration skeleton

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,7 +1,41 @@
 from flask import Flask, request
+from flask_restplus import Api, Resource, fields
+from flask_bcrypt import Bcyrpt
 
 app = Flask(__name__)
+api = Api(app)
+bcrypt = Bcyrpt(app) 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@api.route("/")
+class Hello(Resource):
+    def get(self):
+        return "Hello, World!"
+
+user_model = api.model('User', {
+    'email': fields.String(required=True, description='Email Address', pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"),
+    'password': fields.String(required=True, description='Password')
+})
+
+@api.route("/register")
+@api.doc(params={'email': 'An email address', 'password': 'A valid password'})
+class Register(Resource):
+    @api.expects(user_model, validate=True)
+    @api.response(201, 'User created successfully.')
+    @api.response(400, 'Validation error or user already exists.')
+    def post(self):
+        data = api.payload
+        email = data['email']
+        password = data['password']
+
+        # TODO: check if user exists
+        # if user exists
+        # return {"message": "Email already exists."}, 400
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') 
+        # TODO: store new user in database
+
+        return {"message": "User successfully registered."}, 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
