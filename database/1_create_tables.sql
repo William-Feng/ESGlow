8de69d7296479f57d932e5d5480b0593b8ed7f52
William Feng <williamfeng2001@gmail.com>
2023-10-06 20:49:00 +1100

    F13ATFT-46: Include verification code as an attribute within the users table

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -4,8 +4,9 @@ create database esglow;
 create extension if not exists "uuid-ossp";
 
 create table users (
-    user_id     uuid default uuid_generate_v4(),
-    email       text unique not null,
-    password    text not null,
+    user_id             uuid default uuid_generate_v4(),
+    email               text unique not null,
+    password            text not null,
+    verification_code   char(6),
     primary key (user_id)
 );
