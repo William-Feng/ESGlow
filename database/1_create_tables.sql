1e7d85452ce0206912f92cb73b4f591cef4dd2de
William Feng <williamfeng2001@gmail.com>
2023-10-06 20:38:15 +1100

    F13ATFT-46: Rename attribute name to 'password'

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -2,8 +2,8 @@ create database esglow;
 \c esglow;
 
 create table users (
-    user_id         uuid,
-    email           text unique not null,
-    password_hash   text not null,
+    user_id     uuid,
+    email       text unique not null,
+    password    text not null,
     primary key (user_id)
 );
