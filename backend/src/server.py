d4b9e17859e780d026530e353ad03fefa7002264
seanwibi1 <seanwibi.work@gmail.com>
2023-10-06 13:11:18 +1100

    F13ATFT-32: Switched to flask-restx to support later versions of Flask

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
-from flask_restplus import Api, Resource, fields
-from flask_bcrypt import Bcyrpt
+from flask_restx import Api, Resource, fields
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 api = Api(app)
-bcrypt = Bcyrpt(app) 
+bcrypt = Bcrypt(app) 
 
 @api.route("/")
 class Hello(Resource):
@@ -12,14 +12,14 @@ class Hello(Resource):
         return "Hello, World!"
 
 user_model = api.model('User', {
-    'email': fields.String(required=True, description='Email Address', pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"),
+    'email': fields.String(required=True, description='Email Address', pattern=r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"),
     'password': fields.String(required=True, description='Password')
 })
 
 @api.route("/register")
 @api.doc(params={'email': 'An email address', 'password': 'A valid password'})
-class Register(Resource):
-    @api.expects(user_model, validate=True)
+class RegisterUser(Resource):
+    @api.expect(user_model, validate=True)
     @api.response(201, 'User created successfully.')
     @api.response(400, 'Validation error or user already exists.')
     def post(self):
