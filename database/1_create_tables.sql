45b8aac0d636607d35170fb4b85c77fabe3babb4
seanwibi1 <seanwibi.work@gmail.com>
2023-10-06 15:28:51 +1100

    F13ATFT-32: Generate uuid upon registration

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -2,7 +2,7 @@ create database esglow;
 \c esglow;
 
 create table users (
-    user_id         serial,
+    user_id         uuid,
     email           text unique not null,
     password_hash   text not null,
     primary key (user_id)
