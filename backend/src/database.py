9b5299fd3190cbcd3c10f18d4dfcadadbc28720d
William Feng <williamfeng2001@gmail.com>
2023-10-06 21:12:49 +1100

    F13ATFT-46: Update the User class to reflects the changes in the SQL table definition

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,5 +1,7 @@
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -7,6 +9,7 @@ bcrypt = Bcrypt()
 class User(db.Model):
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    password_hash = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    verification_code = db.Column(db.String(length=6))
