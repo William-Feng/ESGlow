c5e1815b3372328bf9871faa67cfa51d92a93884
William Feng <williamfeng2001@gmail.com>
2023-10-06 20:41:25 +1100

    F13ATFT-46: Ensure the uuid's for user_id are generated by default

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -1,8 +1,10 @@
 create database esglow;
 \c esglow;
 
+create extension if not exists "uuid-ossp";
+
 create table users (
-    user_id     uuid,
+    user_id     uuid default uuid_generate_v4(),
     email       text unique not null,
     password    text not null,
     primary key (user_id)
