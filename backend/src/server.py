eb22cb07e03407deca214f5b33d4b4c45d2c31c3
seanwibi1 <seanwibi.work@gmail.com>
2023-10-06 15:28:51 +1100

    F13ATFT-32: Generate uuid upon registration

--- backend/src/server.py
+++ backend/src/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_bcrypt import Bcrypt
 from flask_restx import Api, Resource, fields
 from flask_sqlalchemy import SQLAlchemy
+import uuid
 
 
 app = Flask(__name__)
@@ -17,7 +18,7 @@ class Hello(Resource):
         return "Hello, World!"
 
 user_model = api.model('User', {
-    'email': fields.String(required=True, description='Email Address', pattern=r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"),
+    'email': fields.String(required=True, description='Email Address', example="william.feng@gmail.com"),
     'password': fields.String(required=True, description='Password')
 })
 
@@ -25,7 +26,7 @@ user_model = api.model('User', {
 class User(db.Model):
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
 
@@ -45,10 +46,12 @@ class RegisterUser(Resource):
         if existing_user:
             return {"message": "Email already exists."}, 400
 
+        # Generate password hash and uuid
         password_hash = bcrypt.generate_password_hash(password).decode('utf-8') 
+        uid = uuid.uuid4()
         
         # Store new user in database
-        new_user = User(email=email, password_hash=password_hash)
+        new_user = User(user_id=uid, email=email, password_hash=password_hash)
         db.session.add(new_user)
         db.session.commit()
 
