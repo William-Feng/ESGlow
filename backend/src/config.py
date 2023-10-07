21366082ff5998b2a10d8d60a8a33df9fb49f9f3
William Feng <williamfeng2001@gmail.com>
2023-10-07 23:00:28 +1100

    F13ATFT-33: Refactor code for extensibility and readability

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,5 +1,1 @@
-DATABASE_URI = 'postgresql://postgres:postgres@localhost:54321/esglow'
-
-TEST_DATABASE_NAME = 'esglow_test'
-TEST_DATABASE_URI = f'postgresql://postgres:postgres@localhost:54321/{TEST_DATABASE_NAME}'
-DEFAULT_DB_URI = 'postgresql://postgres:postgres@localhost:54321/postgres'
+DATABASE_URL = 'postgresql://postgres:postgres@localhost:54321/esglow'
