5cb67c2736bc4aec1adff825ce0f4d5d25b94ca6
Carly Lim <carlylim@192-168-1-105.tpgi.com.au>
2023-10-09 01:13:10 +1100

    F13ATFT-47: Refactored the frontend code, separating general landing page and its components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from './components/Register'
-import Login from './components/Login';
 import CssBaseLine from '@mui/material/CssBaseline'
+import StartPage from './components/StartPage';
 
 function App() {
-  	return (
-		<BrowserRouter >
-		<CssBaseLine />
-			<Routes>
-				<Route exact path="/" element={<Login />} /> {/* change to login */}
-				<Route path="/register" element={<Register />} />
-			</Routes>
-		</BrowserRouter>
+  return (
+    <BrowserRouter >
+    <CssBaseLine />
+      <Routes>
+        <Route exact path="/" element={<StartPage page={ 'login' }/>} />
+        <Route path="/register" element={<StartPage page={ 'register' }/>} />
+        <Route path="/resetPassword" element={<StartPage page={ 'reset' }/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
