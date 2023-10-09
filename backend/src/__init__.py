bb860b2a302fd1d65457078e4f556ce0d2fa0fd4
William Feng <williamfeng2001@gmail.com>
2023-10-09 17:35:01 +1100

    F13ATFT-51: Setup JWT configuration

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+import os
 
 # User-defined module imports
 from .database import db, bcrypt
@@ -8,9 +10,11 @@ from . import server, config
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
+    app.config['JWT_SECRET_KEY'] = os.urandom(32).hex()
 
     bcrypt.init_app(app)
     db.init_app(app)
+    JWTManager.init_app(app)
     server.api.init_app(app)
 
     return app
--- backend/src/config.py
+++ backend/src/config.py
@@ -1,1 +1,4 @@
 DATABASE_URL = 'postgresql://postgres:postgres@localhost:54321/esglow'
+
+# Note that this is NOT good practice and should be stored as an environment variable
+JWT_SECRET_KEY = '08edf839500fddd34689a08ba8c24e2ffa7ad8a0f0a5a78db02b50cda07c7b6c'
