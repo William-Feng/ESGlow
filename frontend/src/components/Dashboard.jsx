cea694682778010a342fba0880a770dc26911414
z5311248 <melanie.wu@student.unsw.edu.au>
2023-10-10 22:38:18 +1100

    F13ATFT-59: Create layout for dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,33 @@
+import { AppBar, Box, CssBaseline, ThemeProvider, Toolbar, createTheme } from '@mui/material'
 import React from 'react'
+import Header from './Header';
 
-export default function Dashboard ({ token }) {
+function Dashboard ({ token }) {
+  const defaultTheme = createTheme();
+  
   return (
-    <div>Dashboard</div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar
+          enableColorOnDark
+          position='fixed'
+          color='inherit'
+          elevation={0}
+          sx={{
+            bgcolor: 'primary.main',
+            height: 128,
+          }}
+        >
+          <Toolbar>
+            <Header />
+          </Toolbar>
+        </AppBar>
+        {/* sidebar */}
+        {/* table */}
+      </Box>
+    </ThemeProvider>
+  );
 }
+
+export default Dashboard;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { Box, IconButton, InputBase, Paper, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import React from 'react';
+import Logo from "../assets/logo-white.png"
+
+function Header () {
+  const [view, setView] = React.useState('single');
+
+  const handleView = (event, newView) => {
+    setView(newView);
+  };
+
+  return (
+    <>
+      <Box
+        component='img'
+        sx={{
+          height: 64,
+        }}
+        alt='logo'
+        src={Logo}
+      />
+      <Paper
+        component='form'
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      >
+        <IconButton sx={{ p: '10px' }} aria-label='menu'>
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex:1 }}
+          placeholder='Select a company'
+          inputProps={{ 'aria-label': 'select a company' }}
+        />
+      </Paper>
+      <ToggleButtonGroup
+        value={view}
+        exclusive
+        onChange={handleView}
+        aria-label='company view'
+      >
+        <ToggleButton value='single'>
+          <Typography>Single Company View</Typography>
+        </ToggleButton>
+        <ToggleButton value='multiple'>
+          <Typography>Comparison View</Typography>
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </>
+  );
+}
+
+export default Header;
