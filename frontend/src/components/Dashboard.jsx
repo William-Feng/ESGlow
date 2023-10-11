e221967851f804a643f56d3196cc366f3b3dd8ab
Carly Lim <carlylim@192-168-1-105.tpgi.com.au>
2023-10-12 02:28:05 +1100

    F13ATFT-64: Creating general framework of sections and added accordion

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,8 @@
-import { AppBar, Box, CssBaseline, ThemeProvider, Toolbar, createTheme } from '@mui/material'
+import { AppBar, Box, CssBaseline, Drawer, ThemeProvider, Toolbar, createTheme } from '@mui/material'
 import React from 'react'
 import Header from './Header';
 import Searchbar from './Searchbar';
+import SelectionSidebar from './SelectionSidebar';
 
 function Dashboard ({ token }) {
   const defaultTheme = createTheme();
@@ -28,9 +29,36 @@ function Dashboard ({ token }) {
             <Searchbar/>
           </Toolbar>
         </AppBar>
-        {/* should also have company overview */}
-        {/* sidebar */}
-        {/* table */}
+        <Box
+          sx={{ position: 'fixed', top: '128px', width: '100%' }}
+        >
+          <Box
+            sx={{ border: 'dotted', margin: '0', textAlign: 'center', height: '200px' }}
+          >
+            {/* should also have company overview */}
+            Company Overview is here.
+          </Box>
+          <Box>
+            <Drawer
+              sx={{
+                position: 'static',
+                width: 240,
+                flexShrink: 0,
+                top: '328px',
+                '& .MuiDrawer-paper': {
+                  width: 240,
+                  boxSizing: 'border-box',
+                },
+              }}
+              variant="permanent"
+              anchor="left"
+            >
+             <SelectionSidebar />
+            </Drawer>
+            
+            {/* table */}
+          </Box>
+        </Box>
       </Box>
     </ThemeProvider>
   );
