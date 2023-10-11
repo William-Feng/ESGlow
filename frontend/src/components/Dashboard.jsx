423a76bff61c089e64bdacd6d2e9b1bfe532e8b2
Carly Lim <carlylim@192-168-1-105.tpgi.com.au>
2023-10-12 01:45:40 +1100

    F13ATFT-59: Create basic UI layout of top section of dashboard, including searchbar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Box, CssBaseline, ThemeProvider, Toolbar, createTheme } from '@mui/material'
 import React from 'react'
 import Header from './Header';
+import Searchbar from './Searchbar';
 
 function Dashboard ({ token }) {
   const defaultTheme = createTheme();
@@ -17,12 +18,17 @@ function Dashboard ({ token }) {
           sx={{
             bgcolor: 'primary.main',
             height: 128,
+            margin: ''
           }}
         >
           <Toolbar>
             <Header />
           </Toolbar>
+          <Toolbar sx={{margin: 'auto'}}>
+            <Searchbar/>
+          </Toolbar>
         </AppBar>
+        {/* should also have company overview */}
         {/* sidebar */}
         {/* table */}
       </Box>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import { Box, IconButton, InputBase, Paper, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import { Box, Typography, Tooltip, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
 import React from 'react';
 import Logo from "../assets/logo-white.png"
 
 function Header () {
-  const [view, setView] = React.useState('single');
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleView = (event, newView) => {
-    setView(newView);
+  const handleOpenUserMenu = (e) => {
+    setAnchorElUser(e.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
@@ -20,32 +23,50 @@ function Header () {
         alt='logo'
         src={Logo}
       />
-      <Paper
-        component='form'
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 3,
+          display: { xs: 'none', md: 'flex' },
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'white',
+          textDecoration: 'none',
+        }}
       >
-        <IconButton sx={{ p: '10px' }} aria-label='menu'>
-          <SearchIcon />
-        </IconButton>
-        <InputBase
-          sx={{ ml: 1, flex:1 }}
-          placeholder='Select a company'
-          inputProps={{ 'aria-label': 'select a company' }}
-        />
-      </Paper>
-      <ToggleButtonGroup
-        value={view}
-        exclusive
-        onChange={handleView}
-        aria-label='company view'
-      >
-        <ToggleButton value='single'>
-          <Typography>Single Company View</Typography>
-        </ToggleButton>
-        <ToggleButton value='multiple'>
-          <Typography>Comparison View</Typography>
-        </ToggleButton>
-      </ToggleButtonGroup>
+        ESGLOW
+      </Typography>
+      <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
+        <Tooltip title="Open settings">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem key='Account' onClick={handleCloseUserMenu}>
+            <Typography textAlign="center">Account</Typography>
+          </MenuItem>
+        </Menu>
+      </Box>
     </>
   );
 }
