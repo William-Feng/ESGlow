1f4856109896630471c99bde40d810d6e4162d39
William Feng <williamfeng2001@gmail.com>
2023-10-11 22:33:48 +1100

    F13ATFT-56: Add tables for the many-to-many relationships and modify primary keys to be 'serial' for simplicity

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -12,27 +12,27 @@ CREATE TABLE users (
 );
 
 CREATE TABLE companies (
-    company_id          UUID DEFAULT uuid_generate_v4(),
+    company_id          SERIAL,
     name                TEXT UNIQUE NOT NULL,
     PRIMARY KEY (company_id)
 );
 
 CREATE TABLE frameworks (
-    framework_id        UUID DEFAULT uuid_generate_v4(),
+    framework_id        SERIAL,
     name                TEXT UNIQUE NOT NULL,
     description         TEXT,
     PRIMARY KEY (framework_id)
 );
 
 CREATE TABLE metrics (
-    metric_id           UUID DEFAULT uuid_generate_v4(),
+    metric_id           SERIAL,
     name                TEXT NOT NULL,
     description         TEXT,
     PRIMARY KEY (metric_id)
 );
 
 CREATE TABLE indicators (
-    indicator_id        UUID DEFAULT uuid_generate_v4(),
+    indicator_id        SERIAL,
     name                TEXT NOT NULL,
     description         TEXT,
     source              TEXT,
@@ -41,10 +41,22 @@ CREATE TABLE indicators (
 );
 
 CREATE TABLE values (
-    value_id            UUID DEFAULT uuid_generate_v4(),
-    indicator_id        UUID REFERENCES Indicators(indicator_id),
-    company_id          UUID REFERENCES Companies(company_id),
+    value_id            SERIAL,
+    indicator_id        INT REFERENCES Indicators(indicator_id),
+    company_id          INT REFERENCES Companies(company_id),
     year                INT,
     value               FLOAT,
     PRIMARY KEY (value_id)
+);
+
+CREATE TABLE framework_metrics (
+    framework_id        INT REFERENCES frameworks(framework_id),
+    metric_id           INT REFERENCES metrics(metric_id),
+    PRIMARY KEY (framework_id, metric_id)
+);
+
+CREATE TABLE metric_indicators (
+    metric_id           INT REFERENCES metrics(metric_id),
+    indicator_id        INT REFERENCES indicators(indicator_id),
+    PRIMARY KEY (metric_id, indicator_id)
 );
