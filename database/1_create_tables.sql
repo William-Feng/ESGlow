a3c50d3904625823ca97f6ed882938db3b9d6cb8
William Feng <williamfeng2001@gmail.com>
2023-10-11 22:39:20 +1100

    F13ATFT-56: Add constraints and future user preferences table to finalise draft of entire database schema

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -6,7 +6,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
     user_id             UUID DEFAULT uuid_generate_v4(),
     email               TEXT UNIQUE NOT NULL,
-    password            TEXT NOT NULL,
+    password            TEXT NOT NULL,      -- Note that the password hash is stored instead
     verification_code   CHAR(6),
     PRIMARY KEY (user_id)
 );
@@ -14,6 +14,7 @@ CREATE TABLE users (
 CREATE TABLE companies (
     company_id          SERIAL,
     name                TEXT UNIQUE NOT NULL,
+    description         TEXT,
     PRIMARY KEY (company_id)
 );
 
@@ -36,15 +37,15 @@ CREATE TABLE indicators (
     name                TEXT NOT NULL,
     description         TEXT,
     source              TEXT,
-    weight              FLOAT,
+    weight              FLOAT CHECK (weight >= 0 AND weight <= 1),
     PRIMARY KEY (indicator_id)
 );
 
-CREATE TABLE values (
+CREATE TABLE data_values (
     value_id            SERIAL,
     indicator_id        INT REFERENCES Indicators(indicator_id),
     company_id          INT REFERENCES Companies(company_id),
-    year                INT,
+    year                INT CHECK (year > 1900 AND year <= 2030),
     value               FLOAT,
     PRIMARY KEY (value_id)
 );
@@ -59,4 +60,15 @@ CREATE TABLE metric_indicators (
     metric_id           INT REFERENCES metrics(metric_id),
     indicator_id        INT REFERENCES indicators(indicator_id),
     PRIMARY KEY (metric_id, indicator_id)
+);
+
+CREATE TABLE user_preferences (
+    preference_id       SERIAL,
+    user_id             UUID REFERENCES users(user_id),
+    framework_id        INT REFERENCES Frameworks(framework_id),
+    metric_id           INT REFERENCES Metrics(metric_id),
+    indicator_id        INT REFERENCES Indicators(indicator_id),
+    weight              FLOAT CHECK (weight >= 0 AND weight <= 1),
+    saved_date          DATE,
+    PRIMARY KEY (preference_id)
 );
