6df08dd48fb6a29e1f462885e7ab675c53cffd38
William Feng <williamfeng2001@gmail.com>
2023-10-11 22:25:18 +1100

    F13ATFT-56: Create tables to represent the main entities required for this project

--- database/1_create_tables.sql
+++ database/1_create_tables.sql
@@ -10,3 +10,41 @@ CREATE TABLE users (
     verification_code   CHAR(6),
     PRIMARY KEY (user_id)
 );
+
+CREATE TABLE companies (
+    company_id          UUID DEFAULT uuid_generate_v4(),
+    name                TEXT UNIQUE NOT NULL,
+    PRIMARY KEY (company_id)
+);
+
+CREATE TABLE frameworks (
+    framework_id        UUID DEFAULT uuid_generate_v4(),
+    name                TEXT UNIQUE NOT NULL,
+    description         TEXT,
+    PRIMARY KEY (framework_id)
+);
+
+CREATE TABLE metrics (
+    metric_id           UUID DEFAULT uuid_generate_v4(),
+    name                TEXT NOT NULL,
+    description         TEXT,
+    PRIMARY KEY (metric_id)
+);
+
+CREATE TABLE indicators (
+    indicator_id        UUID DEFAULT uuid_generate_v4(),
+    name                TEXT NOT NULL,
+    description         TEXT,
+    source              TEXT,
+    weight              FLOAT,
+    PRIMARY KEY (indicator_id)
+);
+
+CREATE TABLE values (
+    value_id            UUID DEFAULT uuid_generate_v4(),
+    indicator_id        UUID REFERENCES Indicators(indicator_id),
+    company_id          UUID REFERENCES Companies(company_id),
+    year                INT,
+    value               FLOAT,
+    PRIMARY KEY (value_id)
+);
