830c144eec33ed23cc824522b75bb9cde0f06fbe
Richard Xue <z5358698@ad.unsw.edu.au>
2023-11-04 22:11:52 +1100

    F13ATFT-92: Cleaned up TODOS in code; Industry values w/ JWT added

--- backend/src/calculations.py
+++ backend/src/calculations.py
@@ -1,5 +1,15 @@
-from .database import db, Company, Industry, Framework, Metric, Indicator, DataValue, CompanyFramework, FrameworkMetric, MetricIndicator
-
+from .database import (
+    db,
+    Company,
+    Industry,
+    Framework,
+    Metric,
+    Indicator,
+    DataValue,
+    CompanyFramework,
+    FrameworkMetric,
+    MetricIndicator,
+)
 
 
 def get_company_values(companies):
@@ -11,7 +21,7 @@ def get_company_values(companies):
 
     Returns:
         {
-            company_id: 
+            company_id:
             {
                 id: Company Id
                 ESGscore: Int
@@ -24,7 +34,7 @@ def get_company_values(companies):
     for company in companies:
         values[company] = get_company_value(company)
     return values
-    
+
 
 def get_company_value(company):
     """
@@ -40,23 +50,27 @@ def get_company_value(company):
             frameworks: [ {name: , score: } ...]
         }
     """
-    
+
     # Find All Frameworks -> Metrics -> Indicators
-    
+
     frameworks = (
         db.session.query(Framework)
         .join(CompanyFramework, Framework.framework_id == CompanyFramework.framework_id)
         .filter(CompanyFramework.company_id == company)
         .all()
     )
-    
+
     metrics = (
         db.session.query(Metric)
         .join(FrameworkMetric, Metric.metric_id == FrameworkMetric.metric_id)
-        .filter(FrameworkMetric.framework_id.in_([framework.framework_id for framework in frameworks]))
+        .filter(
+            FrameworkMetric.framework_id.in_(
+                [framework.framework_id for framework in frameworks]
+            )
+        )
         .all()
     )
-    
+
     indicators = (
         db.session.query(Indicator)
         .join(MetricIndicator, Indicator.indicator_id == MetricIndicator.indicator_id)
@@ -69,36 +83,42 @@ def get_company_value(company):
         db.session.query(db.func.max(DataValue.year))
         .filter(
             DataValue.company_id == company,
-            DataValue.indicator_id.in_([indicator.indicator_id for indicator in indicators]),
+            DataValue.indicator_id.in_(
+                [indicator.indicator_id for indicator in indicators]
+            ),
         )
         .scalar()
     )
-    
- 
+
     # Calculate the data values of each indicator, then the data values of all metrics.
     metric_values = {}
     for metric in metrics:
-        metric_values[metric.metric_id] = calculate_metric(metric, most_recent_year, company)
-        
+        metric_values[metric.metric_id] = calculate_metric(
+            metric, most_recent_year, company
+        )
+
     # Calculate the value of each framework.
     framework_values = {}
     for framework in frameworks:
         framework_values[framework.framework_id] = {
-            'framework_id' : framework.framework_id,
-            'name' : framework.name,
-            'score' : calculate_framework(framework, metric_values)
+            "framework_id": framework.framework_id,
+            "name": framework.name,
+            "score": calculate_framework(framework, metric_values),
         }
-        
+
     return {
-        'message' : "Values for company retrieved!",
-        'value' : {
-            'id': company,
-            'ESGscore': sum([framework['score'] for framework in framework_values.values()])//len(framework_values),
-            'year': most_recent_year,
-            'frameworks': [framework_values[key] for key in framework_values.keys()]
-        }        
+        "message": "Values for company retrieved!",
+        "value": {
+            "id": company,
+            "ESGscore": sum(
+                [framework["score"] for framework in framework_values.values()]
+            )
+            // len(framework_values),
+            "year": most_recent_year,
+            "frameworks": [framework_values[key] for key in framework_values.keys()],
+        },
     }
-    
+
 
 def calculate_framework(framework, metric_values):
     """
@@ -106,26 +126,25 @@ def calculate_framework(framework, metric_values):
     Multiply the predefined weight by the metric values, and return the sum.
 
     Args:
-        framework (Framework): 
-        metric_values ([int]): 
+        framework (Framework):
+        metric_values ([int]):
     Return:
         framework_value(int)
     """
-    
+
     framework_metrics = (
         db.session.query(Metric, FrameworkMetric.predefined_weight)
         .join(FrameworkMetric, Metric.metric_id == FrameworkMetric.metric_id)
         .filter(FrameworkMetric.framework_id == framework.framework_id)
         .all()
     )
-    
+
     framework_value = 0
     for metric, weight in framework_metrics:
         framework_value += metric_values[metric.metric_id] * weight
-    
+
     return framework_value
-    
-    
+
 
 def calculate_metric(metric, most_recent_year, company):
     """
@@ -138,14 +157,14 @@ def calculate_metric(metric, most_recent_year, company):
     Return:
         weighted_sum (int): Sum of data_value of indicator * MetricIndicator.predefined_weight
     """
-    
+
     indicator_weights = (
         db.session.query(Indicator, MetricIndicator.predefined_weight)
         .join(MetricIndicator, Indicator.indicator_id == MetricIndicator.indicator_id)
         .filter(MetricIndicator.metric_id == metric.metric_id)
         .all()
     )
-    
+
     weighted_sum = 0
     for indicator, weight in indicator_weights:
         most_recent_data_value = (
@@ -153,17 +172,17 @@ def calculate_metric(metric, most_recent_year, company):
             .filter(
                 DataValue.company_id == company,
                 DataValue.indicator_id == indicator.indicator_id,
-                DataValue.year == most_recent_year
+                DataValue.year == most_recent_year,
             )
             .scalar()
         )
 
         if most_recent_data_value is not None:
             weighted_sum += most_recent_data_value * weight
-    
+
     return weighted_sum
 
-    
+
 def get_industry_values(industry_id):
     """
     Given a industry_id, return the min, max and average score of the industry.
@@ -172,36 +191,43 @@ def get_industry_values(industry_id):
         industry_id (int): Id for a given Industry
     Return:
         {
-            message: 
+            message:
             min_score: ,
             max_score: ,
-            average_score: 
+            average_score:
         }
     """
     existing_industry = Industry.query.filter_by(industry_id=industry_id).first()
     if not existing_industry:
-        return ({"message" : "Invalid industry id provided"}, 400)
-    
+        return ({"message": "Invalid industry id provided"}, 400)
+
     # Find all companies associated with the industry
-    companies = [company[0] for company in db.session.query(Company.company_id).filter(Company.industry_id == industry_id)]
+    companies = [
+        company[0]
+        for company in db.session.query(Company.company_id).filter(
+            Company.industry_id == industry_id
+        )
+    ]
 
     values = get_company_values(companies)
     scores = []
     for company_value in values.values():
         # Calculate value of all framework_scores
-        company_value = company_value['value']
-        framework_scores = sum([framework["score"] for framework in company_value["frameworks"]])
-        scores.append(framework_scores/len(company_value["frameworks"]))
-        
+        company_value = company_value["value"]
+        framework_scores = sum(
+            [framework["score"] for framework in company_value["frameworks"]]
+        )
+        scores.append(framework_scores / len(company_value["frameworks"]))
+
     if len(scores) == 0:
-        return ({"message" : "Industry has no companies!"}, 400)
-        
-    # May need to extract the average score....
-    return ({
-        "message" : "Values for industry retrieved!",
-        "min_score": min(scores),
-        "max_score": max(scores),
-        "average_score": sum(scores) // len(scores)
-        }, 
-        200)
-    
+        return ({"message": "Industry has no companies!"}, 400)
+
+    return (
+        {
+            "message": "Values for industry retrieved!",
+            "min_score": min(scores),
+            "max_score": max(scores),
+            "average_score": sum(scores) // len(scores),
+        },
+        200,
+    )
--- backend/src/server.py
+++ backend/src/server.py
@@ -2,8 +2,24 @@ from flask_jwt_extended import get_jwt_identity, jwt_required, JWTManager
 from flask_restx import Api, Resource
 
 from .database import User
-from .frameworks import all_industries, all_companies, all_frameworks, all_indicators, get_companies_by_industry, get_framework_info_from_company, get_indicator_values, get_company_info
-from .models import user_authentication_models, password_reset_models, all_industry_company_framework_indicator_models, specific_industry_company_models, framework_metric_indicator_models, value_calculations
+from .frameworks import (
+    all_industries,
+    all_companies,
+    all_frameworks,
+    all_indicators,
+    get_companies_by_industry,
+    get_framework_info_from_company,
+    get_indicator_values,
+    get_company_info,
+)
+from .models import (
+    user_authentication_models,
+    password_reset_models,
+    all_industry_company_framework_indicator_models,
+    specific_industry_company_models,
+    framework_metric_indicator_models,
+    value_calculations,
+)
 from .reset import reset_password_request, reset_password_verify, reset_password_change
 from .user import login, register, get_user
 from .calculations import get_company_values, get_industry_values
@@ -24,13 +40,13 @@ user_model, register_model, login_model = user_authentication_models(api)
 @api.route("/api/register")
 class Register(Resource):
     @api.expect(user_model, validate=True)
-    @api.response(200, 'User created successfully!', model=register_model)
-    @api.response(400, 'User already exists.')
+    @api.response(200, "User created successfully!", model=register_model)
+    @api.response(400, "User already exists.")
     def post(self):
         data = api.payload
-        name = data['name']
-        email = data['email']
-        password = data['password']
+        name = data["name"]
+        email = data["email"]
+        password = data["password"]
 
         return register(name, email, password)
 
@@ -38,12 +54,12 @@ class Register(Resource):
 @api.route("/api/login")
 class Login(Resource):
     @api.expect(user_model, validate=True)
-    @api.response(200, 'Login successful!', model=login_model)
-    @api.response(400, 'Invalid email or password.')
+    @api.response(200, "Login successful!", model=login_model)
+    @api.response(400, "Invalid email or password.")
     def post(self):
         data = api.payload
-        email = data['email']
-        password = data['password']
+        email = data["email"]
+        password = data["password"]
 
         return login(email, password)
 
@@ -54,14 +70,14 @@ def invalid_token_response(invalid_token):
     return {
         "message": "Authentication required. Please log in.",
         "error": "invalid_token",
-        "description": str(invalid_token)
+        "description": str(invalid_token),
     }, 401
 
 
 @api.route("/api/user")
 class DecodeUser(Resource):
-    @api.response(200, 'User authenticated!')
-    @api.response(401, 'Authentication required. Please log in.')
+    @api.response(200, "User authenticated!")
+    @api.response(401, "Authentication required. Please log in.")
     @jwt_required()
     def get(self):
         # Decode the JWT token to retrieve the identity
@@ -76,18 +92,21 @@ class DecodeUser(Resource):
 #
 # ===================================================================
 
-password_reset_request_model, password_reset_verify_model, password_reset_change_model = password_reset_models(
-    api)
+(
+    password_reset_request_model,
+    password_reset_verify_model,
+    password_reset_change_model,
+) = password_reset_models(api)
 
 
 @api.route("/api/password-reset/request")
 class PasswordResetRequest(Resource):
     @api.expect(password_reset_request_model, validate=True)
-    @api.response(200, 'Password Reset Request Successful!')
-    @api.response(400, 'Email does not exist.')
+    @api.response(200, "Password Reset Request Successful!")
+    @api.response(400, "Email does not exist.")
     def post(self):
         data = api.payload
-        email = data['email']
+        email = data["email"]
 
         # Send email, generate code in backend.
         return reset_password_request(email)
@@ -96,13 +115,13 @@ class PasswordResetRequest(Resource):
 @api.route("/api/password-reset/verify")
 class PasswordResetVerify(Resource):
     @api.expect(password_reset_verify_model, validate=True)
-    @api.response(200, 'Password Successfully Reset!')
-    @api.response(400, 'Verification Code is incorrect.')
-    @api.response(400, 'Email does not exist.')
+    @api.response(200, "Password Successfully Reset!")
+    @api.response(400, "Verification Code is incorrect.")
+    @api.response(400, "Email does not exist.")
     def post(self):
         data = api.payload
-        email = data['email']
-        code = data['code']
+        email = data["email"]
+        code = data["code"]
 
         # Verify user exists in backend.
         existing_user = User.query.filter_by(email=email).first()
@@ -116,12 +135,12 @@ class PasswordResetVerify(Resource):
 @api.route("/api/password-reset/change")
 class PasswordResetChange(Resource):
     @api.expect(password_reset_change_model, validate=True)
-    @api.response(200, 'Password Successfully Reset!')
-    @api.response(400, 'Email is incorrect.')
+    @api.response(200, "Password Successfully Reset!")
+    @api.response(400, "Email is incorrect.")
     def post(self):
         data = api.payload
-        email = data['email']
-        new_password = data['new_password']
+        email = data["email"]
+        new_password = data["new_password"]
 
         return reset_password_change(email, new_password)
 
@@ -132,17 +151,24 @@ class PasswordResetChange(Resource):
 #
 # ===================================================================
 
-industry_names_model, company_names_model, framework_names_model, indicator_names_model = all_industry_company_framework_indicator_models(
-    api)
-industry_companies_model, company_info_model = specific_industry_company_models(
-    api)
-framework_detailed_model, indicator_value_detailed_model = framework_metric_indicator_models(
-    api)
+(
+    industry_names_model,
+    company_names_model,
+    framework_names_model,
+    indicator_names_model,
+) = all_industry_company_framework_indicator_models(api)
+industry_companies_model, company_info_model = specific_industry_company_models(api)
+(
+    framework_detailed_model,
+    indicator_value_detailed_model,
+) = framework_metric_indicator_models(api)
+
+
 @api.route("/api/industries/all")
 class IndustriesAll(Resource):
-    @api.response(200, 'All industries retrieved!', model=industry_names_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'No industries found.')
+    @api.response(200, "All industries retrieved!", model=industry_names_model)
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "No industries found.")
     @jwt_required()
     def get(self):
         return all_industries()
@@ -150,9 +176,9 @@ class IndustriesAll(Resource):
 
 @api.route("/api/companies/all")
 class CompaniesAll(Resource):
-    @api.response(200, 'All companies retrieved!', model=company_names_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'No companies found.')
+    @api.response(200, "All companies retrieved!", model=company_names_model)
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "No companies found.")
     @jwt_required()
     def get(self):
         return all_companies()
@@ -160,9 +186,9 @@ class CompaniesAll(Resource):
 
 @api.route("/api/frameworks/all")
 class FrameworksAll(Resource):
-    @api.response(200, 'All frameworks retrieved!', model=framework_names_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'No frameworks found.')
+    @api.response(200, "All frameworks retrieved!", model=framework_names_model)
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "No frameworks found.")
     @jwt_required()
     def get(self):
         return all_frameworks()
@@ -170,9 +196,9 @@ class FrameworksAll(Resource):
 
 @api.route("/api/indicators/all")
 class IndicatorsAll(Resource):
-    @api.response(200, 'All indicators retrieved!', model=indicator_names_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'No indicators found.')
+    @api.response(200, "All indicators retrieved!", model=indicator_names_model)
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "No indicators found.")
     @jwt_required()
     def get(self):
         return all_indicators()
@@ -180,9 +206,13 @@ class IndicatorsAll(Resource):
 
 @api.route("/api/industries/<string:industry_name>")
 class CompaniesByIndustry(Resource):
-    @api.response(200, 'Companies for industry successfully retrieved!', model=industry_companies_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Industry not found.')
+    @api.response(
+        200,
+        "Companies for industry successfully retrieved!",
+        model=industry_companies_model,
+    )
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Industry not found.")
     @jwt_required()
     def get(self, industry_name):
         return get_companies_by_industry(industry_name)
@@ -190,13 +220,12 @@ class CompaniesByIndustry(Resource):
 
 @api.route("/api/companies/<string:company_ids>")
 class CompanyInformation(Resource):
-    @api.response(200, 'Companies\' information successfully retrieved!')
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Company not found.')
+    @api.response(200, "Companies' information successfully retrieved!")
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Company not found.")
     def get(self, company_ids):
-
         try:
-            selected_companies = [int(i) for i in company_ids.split(',')]
+            selected_companies = [int(i) for i in company_ids.split(",")]
         except ValueError:
             return {"message": "Invalid company_ids provided."}, 400
 
@@ -205,9 +234,13 @@ class CompanyInformation(Resource):
 
 @api.route("/api/frameworks/<int:company_id>")
 class FrameworksByCompany(Resource):
-    @api.response(200, 'Framework, metric & indicator information for company successfully retrieved!', model=framework_detailed_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Invalid company.')
+    @api.response(
+        200,
+        "Framework, metric & indicator information for company successfully retrieved!",
+        model=framework_detailed_model,
+    )
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Invalid company.")
     @jwt_required()
     def get(self, company_id):
         return get_framework_info_from_company(company_id)
@@ -215,15 +248,18 @@ class FrameworksByCompany(Resource):
 
 @api.route("/api/values/<int:company_id>/<string:indicator_ids>/<string:years>")
 class IndicatorValues(Resource):
-    @api.response(200, 'Indicator values for company successfully retrieved!', model=indicator_value_detailed_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Invalid company, indicator or none found.')
+    @api.response(
+        200,
+        "Indicator values for company successfully retrieved!",
+        model=indicator_value_detailed_model,
+    )
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Invalid company, indicator or none found.")
     @jwt_required()
     def get(self, company_id, indicator_ids, years):
-
         try:
-            selected_indicators = [int(i) for i in indicator_ids.split(',')]
-            selected_years = [int(y) for y in years.split(',')]
+            selected_indicators = [int(i) for i in indicator_ids.split(",")]
+            selected_years = [int(y) for y in years.split(",")]
         except ValueError:
             return {"message": "Invalid indicator_ids or years provided."}, 400
 
@@ -237,42 +273,31 @@ class IndicatorValues(Resource):
 # ===================================================================
 
 
-# TODO: Return Max/Min/Average of a Framework.
-# Given Framework, find most recent year of data for framework.
-# For year, find all companies w/ Year in that Framework.
-# Calculate the Max, Min, Mean
-# TODO: Could also do calc with company later
-
 company_values_model = value_calculations(api)
+
 
 @api.route("/api/values/<string:company_id>")
 class CompanyValues(Resource):
-    # TODO: Model is broken? Unknown why...
     @api.response(200, "Values for company retrieved!", model=company_values_model)
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Invalid company id provided')
-        
-    
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Invalid company id provided")
     @jwt_required()
     def get(self, company_id):
         try:
-            selected_companies = [int(i) for i in company_id.split(',')]
+            selected_companies = [int(i) for i in company_id.split(",")]
         except ValueError:
             return {"message": "Invalid company id provided"}, 400
 
-        
         return get_company_values(selected_companies), 200
-       
-       
-       
+
+
 @api.route("/api/values/<int:industry_id>")
 class IndustryValues(Resource):
-    # TODO: Model is broken? Unknown why...
+    # TODO: Model has been removed for error
     @api.response(200, "Values for industry retrieved!")
-    @api.response(401, 'Authentication required. Please log in.')
-    @api.response(400, 'Invalid industry id provided')
-    @api.response(400, 'Industry has no companies!')
-        
-    #@jwt_required()
+    @api.response(401, "Authentication required. Please log in.")
+    @api.response(400, "Invalid industry id provided")
+    @api.response(400, "Industry has no companies!")
+    @jwt_required()
     def get(self, industry_id):
         return get_industry_values(industry_id)
