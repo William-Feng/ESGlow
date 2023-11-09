2ffcdd0c01beebf3dee7a55d0b821725d068fc92
Carly Lim <s.carlylim@gmail.com>
2023-11-10 01:00:27 +1100

    F13ATFT-25: Displaying graph finally after figuring out dependencies and library

--- frontend/src/pages/Dashboard/ComparisonView/ComparisonGraph.jsx
+++ frontend/src/pages/Dashboard/ComparisonView/ComparisonGraph.jsx
@@ -1,23 +1,43 @@
 import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import { ChartContainer } from '@mui/x-charts';
+import { LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
 
-export default function SimpleCharts() {
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
+
+function ComparisonGraph() {
   return (
-    <BarChart
-      xAxis={[
-        {
-          id: 'barCategories',
-          data: ['bar A', 'bar B', 'bar C'],
-          scaleType: 'band',
-        },
-      ]}
-      series={[
-        {
-          data: [2, 5, 3],
-        },
-      ]}
+    <ChartContainer
       width={500}
       height={300}
-    />
+      series={[{ type: 'line', data: pData }]}
+      xAxis={[{ scaleType: 'point', data: xLabels }]}
+      sx={{
+        '.MuiLineElement-root': {
+          stroke: '#8884d8',
+          strokeWidth: 2,
+        },
+        '.MuiMarkElement-root': {
+          stroke: '#8884d8',
+          scale: '0.6',
+          fill: '#fff',
+          strokeWidth: 2,
+        },
+      }}
+      disableAxisListener
+    >
+      <LinePlot />
+      <MarkPlot />
+    </ChartContainer>
   );
 }
+
+export default ComparisonGraph;
