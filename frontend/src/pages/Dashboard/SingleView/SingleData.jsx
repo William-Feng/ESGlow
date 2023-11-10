2d364b8c7430f6f303415f6a44e7aba601bd6cd8
William Feng <williamfeng2001@gmail.com>
2023-11-10 17:15:17 +1100

    F13ATFT-112: Improve styling of the data sources so they're more readable

--- frontend/src/pages/Dashboard/SingleView/SingleData.jsx
+++ frontend/src/pages/Dashboard/SingleView/SingleData.jsx
@@ -258,17 +258,50 @@ function SingleData() {
                     borderRight: "1px solid",
                     borderColor: "divider",
                     fontSize: "1.1em",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
                   }}
                 >
                   {row.name}
                   <Tooltip
-                    title={row.source.split(";").map((source, index) => (
-                      // Add line break to separate sources
-                      <React.Fragment key={index}>
-                        {source}
-                        <br />
+                    title={
+                      <React.Fragment>
+                        {row.source.split(";").map((source, index) => {
+                          // Splitting the source string into three parts: the source number, name and description
+                          const [sourceNumber, sourceRest] =
+                            source.split(/:(.+)/);
+                          const [sourceName, sourceDescription] =
+                            sourceRest.split(/\((.+)/);
+
+                          return (
+                            <div
+                              key={index}
+                              style={{
+                                marginBottom:
+                                  index < row.source.split(";").length - 1
+                                    ? 10
+                                    : 0,
+                              }}
+                            >
+                              <Typography
+                                component="span"
+                                style={{ fontStyle: "italic" }}
+                              >
+                                {sourceNumber.trim()}:
+                              </Typography>{" "}
+                              <Typography
+                                component="span"
+                                style={{ fontWeight: "bold" }}
+                              >
+                                {sourceName.trim()}
+                              </Typography>
+                              {" (" + sourceDescription.trim()}
+                            </div>
+                          );
+                        })}
                       </React.Fragment>
-                    ))}
+                    }
                     sx={{ marginLeft: "4px" }}
                   >
                     <InfoOutlinedIcon style={{ cursor: "pointer" }} />
