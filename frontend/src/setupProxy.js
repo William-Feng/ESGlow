35261c279270ecd200bc6e7e2604fd18caea5afb
William Feng <williamfeng2001@gmail.com>
2023-11-12 17:10:53 +1100

    F13ATFT-109: Change port to 5001 to avoid conflict with macOS Airplay Receiver

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const target = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const target = process.env.REACT_APP_BACKEND_URL || "http://localhost:5001";
 
 module.exports = function (app) {
   app.use(
