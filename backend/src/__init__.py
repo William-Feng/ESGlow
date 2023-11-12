12e16f4eefd76c4b6318839188127f98e55ecfc6
William Feng <williamfeng2001@gmail.com>
2023-11-12 16:39:01 +1100

    F13ATFT-109: Resolve major issue where Flask did not explicitly bind to all interfaces

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
 
 # User-defined module imports
 from .database import db, bcrypt
@@ -9,12 +8,12 @@ from .config import Config
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URL
     app.config['JWT_SECRET_KEY'] = Config.JWT_SECRET_KEY
 
     bcrypt.init_app(app)
     db.init_app(app)
-    # JWTManager(app)
     server.jwt.init_app(app)
     server.api.init_app(app)
 
