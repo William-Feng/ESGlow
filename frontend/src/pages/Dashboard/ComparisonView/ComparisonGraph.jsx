e67090388b0bc50760c07c32f0de6b74fb8cb958
William Feng <williamfeng2001@gmail.com>
2023-11-15 12:39:48 +1100

    F13ATFT-109: Refactor the comparison graph fetch request

--- frontend/src/pages/Dashboard/ComparisonView/ComparisonGraph.jsx
+++ frontend/src/pages/Dashboard/ComparisonView/ComparisonGraph.jsx
@@ -4,7 +4,10 @@ import { LineChart } from "@mui/x-charts/LineChart";
 import { useContext, useEffect, useState, useRef } from "react";
 import { ComparisonViewContext } from "./ComparisonView";
 import { Box, Button, CircularProgress } from "@mui/material";
-import { useIndicatorMeanScores } from "../../../hooks/UseGraphData";
+import {
+  useIndicatorMeanScores,
+  useComparisonGraphData,
+} from "../../../hooks/UseGraphData";
 
 function ComparisonGraph({ token }) {
   const {
@@ -14,8 +17,12 @@ function ComparisonGraph({ token }) {
     selectedIndicators,
   } = useContext(ComparisonViewContext);
 
-  const [currentData, setCurrentData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { graphData, isLoading, setIsLoading } = useComparisonGraphData(
+    token,
+    selectedCompanies,
+    selectedIndicators,
+    yearsList
+  );
   const [showAverage, setShowAverage] = useState(false);
   const [selectedIndicatorAverage, setSelectedIndicatorAverage] = useState([]);
   const chartRef = useRef(null);
@@ -38,69 +45,6 @@ function ComparisonGraph({ token }) {
     token,
     selectedIndicatorAverage
   );
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    if (selectedCompanies.length === 0 || selectedIndicators.length === 0) {
-      setIsLoading(false);
-      return;
-    }
-
-    const indicatorIds = selectedIndicators.join(",");
-    let yearsListString = yearsList.join(",");
-    const newData = [];
-    const promisesList = [];
-
-    selectedCompanies.forEach((c) => {
-      promisesList.push(
-        fetch(
-          `/api/values/${c.company_id}/${indicatorIds}/${yearsListString}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            const dataValues = data.values;
-
-            const indicatorData = {};
-            // Initialise the indicatorData object
-            selectedIndicators.forEach((indicatorId) => {
-              indicatorData[indicatorId] = {
-                label: `${c.name}`,
-                data: [],
-              };
-            });
-
-            dataValues.forEach((dataValue) => {
-              indicatorData[dataValue.indicator_id].data.push(dataValue.value);
-            });
-
-            selectedIndicators.forEach((indicatorId) => {
-              newData.push(indicatorData[indicatorId]);
-            });
-          })
-          .catch((error) => {
-            console.error(
-              "Error fetching indicator values for company:",
-              error
-            );
-          })
-      );
-    });
-
-    Promise.all(promisesList)
-      .then(() => {
-        setCurrentData(newData);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching all indicator values:", error);
-      });
-  }, [token, selectedCompanies, selectedIndicators, yearsList]);
 
   // Download graph display as SVG
   const handleDownload = () => {
@@ -141,10 +85,9 @@ function ComparisonGraph({ token }) {
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
-
-      setIsLoading(false);
     } catch (error) {
       console.error("Error capturing chart as image:", error);
+    } finally {
       setIsLoading(false);
     }
   };
@@ -166,7 +109,7 @@ function ComparisonGraph({ token }) {
             height={480}
             margin={{ bottom: 100 }}
             series={[
-              ...currentData.map((item) => ({
+              ...graphData.map((item) => ({
                 label: item.label,
                 data: item.data.filter((_, index) =>
                   selectedYearRange.includes(yearsList[index])
